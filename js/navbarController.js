d15bc48053951e9ce7fd6c5e03adbad921f3af40
CMSuisse <cmarti@gmx.net>
2023-03-09 14:41:54 +0100

    Began work on the infos page. Began implementing a feature with which a user can access the SBB timetable.

--- js/navbarController.js
+++ js/navbarController.js
@@ -1,9 +1,5 @@
 // This function toggles the navbar menu on mobile devices when the hamburger menu has been clicked
 function updateNavbar(){
     navbarCollapse = document.getElementById("navbarCollapse");
-    if(navbarCollapse.classList.contains("show")){
-        navbarCollapse.classList.remove("show");
-    } else{
-        navbarCollapse.classList.add("show");
-    }
+    navbarCollapse.classList.toggle("show");
 }
